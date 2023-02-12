24556f2ed3dccdeeda827e0b3cf068bb9a13b8f5
LuisD <luis.domiciano@hotmail.com>
2023-02-11 21:01:04 -0300

    chore: add test:staged script to lint-staged

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,14 @@
+import Dog from "."
+
 it("should return 8", () => {
   expect(2 * 4).toBe(8)
 })
 
 it("should return 6", () => {
   expect(2 * 3).toBe(6)
+})
+
+it("should return 'Auau'", () => {
+  const dog = new Dog()
+  expect(dog.bark()).toBe("Auau")
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Hello World")
+export default class Dog {
+  bark() {
+    return "Auau"
+  }
+}
